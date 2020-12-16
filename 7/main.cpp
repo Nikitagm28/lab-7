dfeb5f14c774eb4c85eb354ed4d9fba2d8f2298d
Nikita <nikita-kuznetsov-2018@list.ru>
2020-12-16 13:33:23 +0300

    7

--- 7/main.cpp
+++ 7/main.cpp
@@ -1,14 +1,63 @@
-//
-//  main.cpp
-//  7
-//
-//  Created by Никита Кузнецов on 16.12.2020.
-//
-
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
+//1 задание:
+//#include<iostream>
+//#include<math.h>
+//using namespace std;
+//int main() {
+//    double P = 3.14, Rad=180, G;
+//    cin >> G;
+//    Rad = G / Rad;
+//    cout << "radians are " << Rad;
+//}
+//2 задание:
+//#include<iostream>
+//#include<math.h>
+//using namespace std;
+//int main() {
+//    double P = 3.14, Rad, G;
+//    cin >> Rad;
+//    G = Rad*180;
+//    cout << "degeese are " << G;
+//}
+//3 задание:
+//#include<iostream>
+//#include<math.h>
+//using namespace std;
+//int main() {
+//    double x, a, y, one;
+//    cin >> x >> a;
+//    one = a / x;
+//    cin >> y;
+//    cout << "\none is " << one << "\ny is " << y*one;
+//}
+//4 задание:
+//#include<iostream>
+//#include<math.h>
+//using namespace std;
+//int main() {
+//    double v1, v2, s, t, ans, v_12;
+//    cin >> v1 >> v2 >> s >> t;
+//    v_12 = v1 + v2;
+//    ans = v_12 * t + s;
+//    cout << ans;
+//}
+//5 задание:
+//#include<iostream>
+//#include<math.h>
+//using namespace std;
+//int main() {
+//    double a, b, x;
+//    cin >> a >> b;
+//    x = -b / a;
+//    cout << x;
+//}
+//6 задание:
+//#include<iostream>
+//#include<math.h>
+//using namespace std;
+//int main() {
+//    double a1, b1, c1, a2, b2, c2, x, y;
+//    cin >> a1 >> b1 >> c1 >> a2 >> b2 >> c2;
+//    y = (c2 * a1 - a2 * c1) / (b2 * a1 - a2 * b1);
+//    x = (c1 - b1 * y) / a1;
+//    cout << "\nX = " << x << "\nY = " << y;
+//}
